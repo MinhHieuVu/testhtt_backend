12fa3933188d6e014b180d797cb2806f9a8dd2c2
VuMinhHieu <minhhieuvux@gmail.com>
2022-01-27 10:18:14 +0700

    adding bootstrap4

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -5,3 +5,18 @@ const vue = require('./loaders/vue')
 environment.plugins.prepend('VueLoaderPlugin', new VueLoaderPlugin())
 environment.loaders.prepend('vue', vue)
 module.exports = environment
+
+// config/webpack/environment.js
+const webpack = require('webpack')
+
+environment.plugins.prepend(
+    'Provide',
+    new webpack.ProvidePlugin({
+        $: 'jquery',
+        jQuery: 'jquery',
+        jquery: 'jquery',
+        'window.jQuery': 'jquery',
+        Popper: ['popper.js', 'default'],
+    })
+)
+
