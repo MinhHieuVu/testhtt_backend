2a97e3e7f6a0b9b964b1b19810462660b7925b22
MinhHieuVux <minhhieuvux@gmail.com>
2022-09-22 21:15:47 +0700

    update

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,6 +2,4 @@ class Category < ApplicationRecord
   has_and_belongs_to_many :tests, join_table: "tests_categories"
   belongs_to :group_category, optional: true
   acts_as_nested_set
-  # acts_as_list scope: :group_category
-
 end
